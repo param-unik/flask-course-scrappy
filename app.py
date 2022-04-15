5c3adf4f9b37b9f8d42c0cc055c1f8760ae949af
param.unik <param.unik@gmail.com>
2022-04-15 14:52:21 +0530

    added logger.info to see errror

--- app.py
+++ app.py
@@ -56,9 +56,9 @@ def index():
                 driver = webdriver.Chrome(executable_path=r'C:\Users\Param\Downloads\ImageScrapper\ImageScrapper\chromedriver.exe', chrome_options=chrome_options)
             else:
                 app.logger.info('Prod environment is activated')
-                # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-                # chrome_options.add_argument("--disable-dev-shm-usage")
-                # chrome_options.add_argument("--no-sandbox")
+                chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+                chrome_options.add_argument("--disable-dev-shm-usage")
+                chrome_options.add_argument("--no-sandbox")
                 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
                 
             # base URL on which web scrape needs to be performed..
